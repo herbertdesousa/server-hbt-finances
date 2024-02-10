1a48fb04ddbf94c01715512acc7bd4b350bfc487
herbertdesousa <sousaherbert138@gmail.com>
2024-02-10 15:17:28 -0300

    FEAT: list and create finances requests

---
+++ src/main/kotlin/com/example/models/Finance.kt
@@ -0,0 +1,36 @@
+package com.example.models
+
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.dao.id.EntityID
+
+enum class FinanceRelativeAt {
+  START_MONTH,
+  END_MONTH
+}
+
+@Serializable
+data class FinanceDTO(
+  val id: Int,
+  var title: String,
+  var relativeAt: FinanceRelativeAt,
+  var distanceDays: Int,
+  var amount: Float,
+)
+
+
+object Finances : BaseTable("finances") {
+  val title = varchar("title", 64)
+  val relativeAt = enumerationByName("relative_at", 11, FinanceRelativeAt::class)
+  val distanceDays = integer("distance_days")
+  val amount = float("amount")
+}
+
+data class Finance(
+  val _id: EntityID<Int>,
+  var title: String,
+  var relativeAt: FinanceRelativeAt,
+  var distanceDays: Int,
+  var amount: Float,
+) : BaseEntity(_id, Finances) {
+  companion object : BaseEntityClass<Finance>(Finances)
+}
