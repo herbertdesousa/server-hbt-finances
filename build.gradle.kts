1eeba495203b13c9fadfd07a9a9dc7aee0f4e2df
herbertdesousa <sousaherbert138@gmail.com>
2024-02-10 15:10:13 -0300

    Revert "FEAT: list and create finances requests"
    This reverts commit 3adaa4e50f8d98a2f739430507904077f6c4de78.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,6 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
-val exposed_version: String by project
-val h2_version: String by project
-val postgresql_version: String by project
-val flyway_version: String by project
-val hikaricp_version: String by project
-
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
@@ -30,24 +24,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-request-validation:$ktor_version")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-
-    // database
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
-    implementation("org.flywaydb:flyway-core:$flyway_version")
-    implementation("org.flywaydb:flyway-database-postgresql:$flyway_version")
-    implementation("org.postgresql:postgresql:$postgresql_version")
-    implementation("com.zaxxer:HikariCP:$hikaricp_version")
-
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/example/routes/FinancesRoutes.kt
+++ src/main/kotlin/com/example/routes/FinancesRoutes.kt
@@ -1,12 +1,11 @@
 package com.example.routes
 
-import com.example.dao.dao
-import com.example.models.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 import java.time.LocalDate
+import kotlin.time.Duration.Companion.days
 
 @Serializable
 data class FinancesDaysResponse(
@@ -16,44 +15,6 @@ data class FinancesDaysResponse(
 
 fun Routing.financesRouting() {
   route("/finances") {
-    get {
-      val finances: List<FinanceDTO> = dao.allFinances().map {
-        FinanceDTO(
-          id = it._id.value,
-          title = it.title,
-          relativeAt = it.relativeAt,
-          distanceDays = it.distanceDays,
-          amount = it.amount,
-        )
-      }
-
-      call.respond(finances)
-    }
-
-    post {
-      val finance: FinanceDTO? = dao.createFinance(
-        title = "Financia I",
-        relativeAt = FinanceRelativeAt.START_MONTH,
-        distanceDays = 5,
-        amount = 20.0f,
-      )?.let {
-        FinanceDTO(
-          id = it._id.value,
-          title = it.title,
-          relativeAt = it.relativeAt,
-          distanceDays = it.distanceDays,
-          amount = it.amount,
-        )
-      }
-
-      if (finance == null) {
-        call.respond(mapOf("error" to "error when creating an finance"))
-        return@post
-      }
-
-      call.respond(finance)
-    }
-
     get("/days") {
       val dayOfMonth = LocalDate.now().dayOfMonth
 
