c181f7f00d6fb50ef4bdab70d186c8febbb89ea4
herbertdesousa <sousaherbert138@gmail.com>
2024-02-10 21:37:50 -0300

    CHORE: 20 to 28 distanceDays max

--- src/main/kotlin/com/example/plugins/Validation.kt
+++ src/main/kotlin/com/example/plugins/Validation.kt
@@ -11,8 +11,8 @@ fun Application.configureValidation() {
         ValidationResult.Invalid("title is required")
       } else if (finance.amount.isNaN() || finance.amount <= 0) {
         ValidationResult.Invalid("amount is required and must be higher than 0")
-      } else if (finance.distanceDays > 20 || finance.distanceDays < 0) {
-        ValidationResult.Invalid("finance is required and between 0 to 20")
+      } else if (finance.distanceDays > 28 || finance.distanceDays < 0) {
+        ValidationResult.Invalid("finance is required and between 0 to 28")
       } else ValidationResult.Valid
     }
   }
